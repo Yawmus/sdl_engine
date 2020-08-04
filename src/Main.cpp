622a3a920123b9de585f5f3bbd1d07deda7933a3
yawmus <peter.jacobsen55@gmail.com>
2020-08-04 03:29:44 -0600

    Got a running win buildgit add .

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,7 @@
 #include "Constants.h"
 #include "Game.h"
 
-extern "C"{
+extern "C" {
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
@@ -11,28 +11,27 @@ extern "C"{
 
 std::string fps = "--";
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	GPU_SetDebugLevel(GPU_DEBUG_LEVEL_2);
-	if(GPU_GetDebugLevel() >= GPU_DEBUG_LEVEL_0){
-		std::cout << "Built with: " << __VERSION__ << std::endl;
+	if (GPU_GetDebugLevel() >= GPU_DEBUG_LEVEL_0) {
 		std::cout << "C++ version: " << __cplusplus << std::endl;
 	}
 
 	// Process args
 	std::string map;
-	if( argc >= 2 )
-    {
+	if (argc >= 2)
+	{
 		map = argv[1];
-    }
+	}
 
-    Game *game = new Game();
+	Game* game = new Game();
 	game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT, map);
 
 	unsigned int delta, ticks, prev_ticks, frame_count = 0, start_time;
-	start_time = SDL_GetTicks();
+	prev_ticks = start_time = SDL_GetTicks();
 
-	while(game->IsRunning())
+	while (game->IsRunning())
 	{
 		ticks = SDL_GetTicks();
 		delta = ticks - prev_ticks;
@@ -43,17 +42,17 @@ int main(int argc, char *argv[])
 
 		prev_ticks = ticks;
 		frame_count++;
-		if (frame_count % 50 == 0){
-			char *buf;
+		if (frame_count % 50 == 0) {
+			char* buf;
 			size_t sz;
-			float temp = 1000.0f*frame_count / (ticks - start_time);
+			float temp = 1000.0f * frame_count / (ticks - start_time);
 			sz = snprintf(NULL, 0, "%.2f", temp);
-			buf = (char *)malloc(sz + 1);
-			snprintf(buf, sz+1, "%.2f", temp);
+			buf = (char*)malloc(sz + 1);
+			snprintf(buf, sz + 1, "%.2f", temp);
 			fps = std::string(buf);
 		}
 	}
-	game->Destroy(); 
+	game->Destroy();
 
-    return 0;
+	return 0;
 }
