f33cc0304746a11917a39b65261460e719f97844
Peter <peter.jacobsen55@gmail.com>
2020-08-04 15:53:50 -0600

    Fixed lua dependency for osx build

---
+++ osx_build/CMakeFiles/INC.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Yawmus/projects/sdl_engine/src/Entity.cpp" "/Users/Yawmus/projects/sdl_engine/osx_build/CMakeFiles/INC.dir/src/Entity.cpp.o"
+  "/Users/Yawmus/projects/sdl_engine/src/Game.cpp" "/Users/Yawmus/projects/sdl_engine/osx_build/CMakeFiles/INC.dir/src/Game.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../start"
+  "../lib/osx/SDL2.framework/Headers"
+  "../lib/osx/SDL2_TTF.framework/Headers"
+  "../lib/osx/SDL2_gpu.framework/Headers"
+  "../lib/osx/lua_5.3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ osx_build/CMakeFiles/INC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/INC.dir/src/Entity.cpp.o"
+  "CMakeFiles/INC.dir/src/Game.cpp.o"
+  "libINC.a"
+  "libINC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/INC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
